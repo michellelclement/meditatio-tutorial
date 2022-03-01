3c995b039383517018692d16695bf8ce0f4d54a3
Michelle Clement <michellelclement@gmail.com>
2022-03-01 10:23:45 +0000

    JS Call all elements for medication app

---
+++ app.js
@@ -0,0 +1,17 @@
+const app = () => {
+    const song = document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    // Get Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    //Get Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    //Get length of outline
+    const outlineLength = outline.getTotalLength();
+    //Duration
+    let fakeDuration = 600;
+}
+
+app();
